24295e83f768b4d96a227522ee95ab1c33a07438
Dmitriy <shevdmitriy2018@gmail.com>
2023-09-19 16:23:34 +0700

    https://github.com/Shevdmitriy2018/SDM.git lkj

---
+++ class auto.py
@@ -0,0 +1,20 @@
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.speed = 0
+
+    def accelerate(self, amount):
+        self.speed += amount
+
+    def brake(self, amount):
+        self.speed -= amount
+
+    def get_speed(self):
+        return f"brand: {self.brand}, model: {self.model}, year: {self.year}, speed: {self.speed}"
+
+car = Car("hyundai", "solaris", "2015")
+car.accelerate(80)
+car.brake(20)
+print(car.get_speed())
